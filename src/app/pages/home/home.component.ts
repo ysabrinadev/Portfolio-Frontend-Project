51e1867568a923f2347e26c20da6a34394f935b6
ysabrinadev <ysabrina.dev@gmail.com>
2024-08-22 19:58:44 -0300

    [Feature] Created Introduction Component V.0.3.1

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
+import { IntroductionComponent } from '../../components/introduction/introduction.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, IntroductionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
