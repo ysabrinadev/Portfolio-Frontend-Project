2eb162d7f68dc36965d1359c7212807074fcc207
ysabrinadev <ysabrina.dev@gmail.com>
2024-09-13 22:46:51 -0300

    [Feature] Created Education Component V.0.4.2

---
+++ src/app/components/educations/educations.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-educations',
+  standalone: true,
+  imports: [],
+  templateUrl: './educations.component.html',
+  styleUrl: './educations.component.scss'
+})
+export class EducationsComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { IntroductionComponent } from '../../components/introduction/introduction.component';
+import { EducationsComponent } from '../../components/educations/educations.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, IntroductionComponent],
+  imports: [HeaderComponent, IntroductionComponent, EducationsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
