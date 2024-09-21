c09f4ee6d8064bb78aaab0f7de5fede0e290e57a
ysabrinadev <ysabrina.dev@gmail.com>
2024-09-21 18:34:24 -0300

    [Feature] Created Testimonials Component V.0.6.4

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,11 +3,12 @@ import { HeaderComponent } from '../../components/header/header.component';
 import { IntroductionComponent } from '../../components/introduction/introduction.component';
 import { EducationsComponent } from '../../components/educations/educations.component';
 import { ServicesComponent } from '../../components/services/services.component';
+import { TestimonialsComponent } from '../../components/testimonials/testimonials.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, IntroductionComponent, EducationsComponent, ServicesComponent],
+  imports: [HeaderComponent, IntroductionComponent, EducationsComponent, ServicesComponent, TestimonialsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
